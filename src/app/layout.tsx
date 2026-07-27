a889e570b9b4615573a0863b494662e8d54f44ab
mateo <levvycum@gmail.com>
2026-07-27 19:10:43 -0300

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// @ts-expect-error CSS import supported by Next.js
 import "./globals.css";
 
 const geistSans = Geist({
